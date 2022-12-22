13b703c3c4436b2a83a069374e989ad61e79ca4e
levau <levau@LAPTOP-9OU4HI5G>
2022-12-22 17:35:37 -0600

    Add project files.

---
+++ Name_Proj.cpp
@@ -0,0 +1,16 @@
+//Aske user to enter their name, returns a statement
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name1, name2;
+
+
+	cout << "Please enter your 1st & last name... " << endl;
+	cin >> name1 >> name2;
+	cout << "Hello....!"  << " "   << name1 << " " << name2;
+
+	return 0;
+}
